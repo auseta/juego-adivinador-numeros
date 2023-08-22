11b8361aae755985333cca58ca6a388b6a132b9d
auseta <alseta97@gmail.com>
2023-08-22 15:07:06 -0300

    Agregar archivos de proyecto.

---
+++ JuegoAdivinadorNumeros/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace JuegoAdivinadorNumeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
